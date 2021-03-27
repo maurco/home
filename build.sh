d7d8e7f475ab1601c24b4a02f0625a52759457a4
Jason Maurer <jsonmaur@fastmail.com>
2021-03-26 19:00:32 -0600

    Build script for Cloudflare

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf dist
+mkdir -p dist
+
+cp normalize.css dist/normalize.css
+cp index.css dist/index.css
+cp index.html dist/index.html
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aws s3 sync . s3://maur.co \
-	--delete \
-	--exclude "*" \
-	--include "normalize.css" \
-	--include "index.css" \
-	--include "index.html" \
-	--profile maur
