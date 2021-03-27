e5195e07f2d2bb5a7a4a8b1e1e24995988166f51
Jason Maurer <jsonmaur@fastmail.com>
2021-03-27 00:47:43 -0600

    Add favicon files

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@
 rm -rf dist
 mkdir -p dist
 
-cp normalize.css dist/normalize.css
-cp index.css dist/index.css
-cp index.html dist/index.html
+cp normalize.css dist/
+cp index.css dist/
+cp index.html dist/
+cp android-chrome-192x192.png dist/
+cp android-chrome-512x512.png dist/
+cp apple-touch-icon.png dist/
+cp browserconfig.xml dist/
+cp favicon-16x16.png dist/
+cp favicon-32x32.png dist/
+cp favicon.ico dist/
+cp mstile-150x150.png dist/
+cp site.webmanifest dist/
